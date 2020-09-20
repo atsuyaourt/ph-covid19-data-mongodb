0508179574b4d9367114ffdbcec130a4167ea208
Emilio Gozo <emil.gozo@gmail.com>
2020-09-20 20:19:05 +0800

    feat: improve make_mappable methods
    - methods can be reused
    - use tqdm to show progress

--- src/make_mappable.py
+++ src/make_mappable.py
@@ -1,4 +1,5 @@
 import os
+from tqdm import tqdm
 from pathlib import Path
 from dotenv import load_dotenv
 from pymongo import MongoClient
@@ -11,7 +12,7 @@ from fuzzywuzzy import process as fz_process
 from models import REGION_MAP, REGION_UNKNOWN
 
 
-def update_loc_city_mun(db_loc_df, mongo_col):
+def update_loc_city_mun(db_loc_df, mongo_col=None):
     muni_city_df = gpd.read_file(Path("input/shp/MuniCity/muni_city.shp"))
     muni_city_df = muni_city_df.loc[
         muni_city_df["type"] != "Waterbody",
@@ -19,57 +20,57 @@ def update_loc_city_mun(db_loc_df, mongo_col):
     muni_city_df = muni_city_df.sort_values("region")
     muni_city_df["loc_name"] = (muni_city_df["name"] + " " + muni_city_df["province"]).str.lower()
 
-    db_loc_df["loc_name"] = (db_loc_df["cityMunRes"] + " " + db_loc_df["provRes"]).str.lower()
+    db_loc_df["loc_name"] = (
+        (db_loc_df["cityMunRes"] + " " + db_loc_df["provRes"]).str.lower().str.replace(r"\([^)]*\)\ ", "", regex=True)
+    )
 
-    tot = db_loc_df.shape[0]
-    cnt = 0
-    for i, r in db_loc_df.iterrows():
-        cnt += 1
+    for i, r in tqdm(db_loc_df.iterrows(), total=db_loc_df.shape[0]):
         m_loc_name = ""
         m_loc_name, _, _ = fz_process.extract(
             r["loc_name"],
             muni_city_df.loc[muni_city_df["region"] == r["regionResGeo"], "loc_name"],
             limit=1,
         )[0]
-        print("{:.2f} %".format(100.0 * cnt / tot))
         if len(m_loc_name) > 0:
             m_prov, m_city_mun = muni_city_df.loc[muni_city_df["loc_name"] == m_loc_name, ["province", "name"]].values[
                 0
             ]
             db_loc_df.loc[db_loc_df["loc_name"] == r["loc_name"], "provResGeo"] = m_prov
             db_loc_df.loc[db_loc_df["loc_name"] == r["loc_name"], "cityMunResGeo"] = m_city_mun
-            mongo_col.update_many(
-                {"regionRes": r["regionRes"], "provRes": r["provRes"], "cityMunRes": r["cityMunRes"]},
-                {"$set": {"regionResGeo": r["regionResGeo"], "provResGeo": m_prov, "cityMunResGeo": m_city_mun}},
-            )
+            if mongo_col is not None:
+                mongo_col.update_many(
+                    {"regionRes": r["regionRes"], "provRes": r["provRes"], "cityMunRes": r["cityMunRes"]},
+                    {"$set": {"regionResGeo": r["regionResGeo"], "provResGeo": m_prov, "cityMunResGeo": m_city_mun}},
+                )
+    if mongo_col is None:
+        return db_loc_df.drop(columns=["loc_name"], errors="ignore")
 
 
-def update_loc_province(db_loc_df, mongo_col):
+def update_loc_province(db_loc_df, mongo_col=None):
     prov_df = gpd.read_file(Path("input/shp/Province/province.shp"))
     prov_df = prov_df.sort_values("region")
 
-    tot = db_loc_df.shape[0]
-    cnt = 0
-    for i, r in db_loc_df.iterrows():
-        cnt += 1
+    for i, r in tqdm(db_loc_df.iterrows(), total=db_loc_df.shape[0]):
         m_loc_name = ""
         m_loc_name, _, _ = fz_process.extract(
             r["provRes"],
             prov_df.loc[prov_df["region"] == r["regionResGeo"], "province"],
             limit=1,
         )[0]
-        print("{:.2f} %".format(100.0 * cnt / tot))
         if len(m_loc_name) > 0:
             m_prov = prov_df.loc[prov_df["province"] == m_loc_name, "province"].values[0]
             db_loc_df.loc[db_loc_df["provRes"] == r["provRes"], "provResGeo"] = m_prov
-            mongo_col.update_many(
-                {"regionRes": r["regionRes"], "provRes": r["provRes"], "cityMunRes": ""},
-                {"$set": {"regionResGeo": r["regionResGeo"], "provResGeo": m_prov}},
-            )
-            mongo_col.update_many(
-                {"regionRes": r["regionRes"], "provRes": r["provRes"], "cityMunRes": {"$exists": False}},
-                {"$set": {"regionResGeo": r["regionResGeo"], "provResGeo": m_prov}},
-            )
+            if mongo_col is not None:
+                mongo_col.update_many(
+                    {"regionRes": r["regionRes"], "provRes": r["provRes"], "cityMunRes": ""},
+                    {"$set": {"regionResGeo": r["regionResGeo"], "provResGeo": m_prov}},
+                )
+                mongo_col.update_many(
+                    {"regionRes": r["regionRes"], "provRes": r["provRes"], "cityMunRes": {"$exists": False}},
+                    {"$set": {"regionResGeo": r["regionResGeo"], "provResGeo": m_prov}},
+                )
+    if mongo_col is None:
+        return db_loc_df
 
 
 def update_loc_region(db_loc_region_df, mongo_col):
