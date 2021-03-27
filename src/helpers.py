9172ba24db482f7662a79c50afd2b8a75724caae
Emilio Gozo <emil.gozo@gmail.com>
2021-03-27 22:50:34 +0800

    reafactor: move several functions and constants to models.py

--- src/helpers.py
+++ src/helpers.py
@@ -1,36 +1,3 @@
-from pandas import to_datetime
-
-from models import CASE_FIELD_MAP, CASE_SCHEMA, CASE_FIELD_DROP
-
-
 # function to convert string to camelCase
 def camel_case(string):
     return string[0].lower() + string[1:]
-
-
-def prep_data(df):
-    df = df.rename(columns=CASE_FIELD_MAP)
-    df.columns = [camel_case(c) for c in df.columns]
-    df = df.drop(columns=CASE_FIELD_DROP, errors="ignore")
-    for col_name in df.columns:
-        if col_name in CASE_SCHEMA:
-            if CASE_SCHEMA[col_name]["dtype"] == "String":
-                df.loc[df[col_name].isna(), col_name] = ""
-                df[col_name] = df[col_name].str.strip()
-            elif CASE_SCHEMA[col_name]["dtype"] == "Integer":
-                df.loc[df[col_name].isna(), col_name] = -1
-                df[col_name] = df[col_name].map(int, na_action="ignore")
-            elif CASE_SCHEMA[col_name]["dtype"] == "Bool":
-                df[col_name] = (
-                    df[col_name].str.strip().str.lower().map({"yes": True, "no": False})
-                )
-            elif CASE_SCHEMA[col_name]["dtype"] == "Date":
-                df.loc[df[col_name].isna(), col_name] = 0
-                df[col_name] = to_datetime(df[col_name]).dt.tz_localize("Asia/Manila")
-            elif CASE_SCHEMA[col_name]["dtype"] == "Enum":
-                df[col_name] = df[col_name].str.strip().str.lower()
-        else:
-            print("Warning: Don't know how to format column: {}".format(col_name))
-            df.loc[df[col_name].isna(), col_name] = ""
-            df[col_name] = df[col_name].str.strip()
-    return df
--- src/models.py
+++ src/models.py
@@ -1,28 +1,33 @@
+from numpy import nan
+from pandas import to_datetime
+
+from helpers import camel_case
+
 SEX_ENUM = ["male", "female"]
 REMOVAL_TYPE_ENUM = ["recovered", "died"]
-HEALTH_STATS_ENUM = ["recovered", "died", "confirmed"]
+HEALTH_STATS_ENUM = ["recovered", "died", "confirmed", "asymptomatic", "mild", "moderate", "severe", "critical"]
 
 CASE_SCHEMA = dict(
-    caseCode=dict(dtype="String", default=""),
-    age=dict(dtype="Integer", default=""),
-    sex=dict(dtype="Enum", default=""),
-    dateSpecimen=dict(dtype="Date", default=0),
-    dateResultRelease=dict(dtype="Date", default=0),
-    dateRepConf=dict(dtype="Date", default=0),
-    dateDied=dict(dtype="Date", default=0),
-    dateRecover=dict(dtype="Date", default=0),
-    removalType=dict(dtype="Enum", default="unknown"),
-    dateRepRem=dict(dtype="Date", default=0),
+    caseCode=dict(dtype="String"),
+    age=dict(dtype="Integer"),
+    sex=dict(dtype="String", choices=SEX_ENUM),
+    dateSpecimen=dict(dtype="Date"),
+    dateResultRelease=dict(dtype="Date"),
+    dateRepConf=dict(dtype="Date"),
+    dateDied=dict(dtype="Date"),
+    dateRecover=dict(dtype="Date"),
+    dateRepRem=dict(dtype="Date"),
+    dateOnset=dict(dtype="Date"),
+    healthStatus=dict(dtype="String", choices=HEALTH_STATS_ENUM),
+    removalType=dict(dtype="String", choices=REMOVAL_TYPE_ENUM),
+    regionRes=dict(dtype="String"),
+    provRes=dict(dtype="String"),
+    cityMunRes=dict(dtype="String"),
+    cityMuniPSGC=dict(dtype="String"),
+    barangayRes=dict(dtype="String"),
+    barangayPSGC=dict(dtype="String"),
     isAdmitted=dict(dtype="Bool"),
-    regionRes=dict(dtype="String", default=""),
-    provRes=dict(dtype="String", default=""),
-    cityMunRes=dict(dtype="String", default=""),
-    cityMuniPSGC=dict(dtype="String", default=""),
-    barangayRes=dict(dtype="String", default=""),
-    barangayPSGC=dict(dtype="String", default=""),
-    healthStatus=dict(dtype="Enum", default="unknown"),
     isQuarantined=dict(dtype="Bool"),
-    dateOnset=dict(dtype="Date", default=0),
     isPregnant=dict(dtype="Bool"),
 )
 
@@ -58,3 +63,45 @@ REGION_MAP = {
 }
 
 REGION_UNKNOWN = ["ROF", "Repatriate", ""]
+
+def prep_cases_df(df):
+    df = df.rename(columns=CASE_FIELD_MAP)
+    df.columns = [camel_case(c) for c in df.columns]
+    df = df.drop(columns=CASE_FIELD_DROP, errors="ignore")
+    valid = True
+    for col_name in df.columns:
+        if col_name in CASE_SCHEMA:
+            if CASE_SCHEMA[col_name]["dtype"] == "String":
+                if "choices" in CASE_SCHEMA[col_name]:
+                    df[col_name] = df[col_name].str.strip().str.lower()
+                    no_match = df.dropna().loc[~df[col_name].isin(CASE_SCHEMA[col_name]["choices"]), col_name].unique()
+                    if len(no_match) > 0:
+                        print(f"{col_name} has unmatched values: {no_match}!!! exiting...")
+                        valid = False
+                        break
+                if "default" in CASE_SCHEMA[col_name]:
+                    df.loc[df[col_name].isna(), col_name] = CASE_SCHEMA[col_name]["default"]
+                else:
+                    df.loc[df[col_name].isna(), col_name] = ""
+                df[col_name] = df[col_name].str.strip()
+            elif CASE_SCHEMA[col_name]["dtype"] == "Integer":
+                if "default" in CASE_SCHEMA[col_name]:
+                    df.loc[df[col_name].isna(), col_name] = CASE_SCHEMA[col_name]["default"]
+                else:
+                    df.loc[df[col_name].isna(), col_name] = -1
+                df[col_name] = df[col_name].map(int, na_action="ignore")
+            elif CASE_SCHEMA[col_name]["dtype"] == "Bool":
+                df[col_name] = (
+                    df[col_name].str.strip().str.lower().map({"yes": True, "no": False})
+                )
+            elif CASE_SCHEMA[col_name]["dtype"] == "Date":
+                df[col_name] = to_datetime(df[col_name]).dt.tz_localize("Asia/Manila")
+                if "default" in CASE_SCHEMA[col_name]:
+                    df.loc[df[col_name].isna(), col_name] = CASE_SCHEMA[col_name]["default"]
+                else:
+                    df.loc[df[col_name].isna(), col_name] = 0
+        else:
+            print("Warning: Don't know how to format column: {}".format(col_name))
+            df.loc[df[col_name].isna(), col_name] = ""
+            df[col_name] = df[col_name].str.strip()
+    return df
