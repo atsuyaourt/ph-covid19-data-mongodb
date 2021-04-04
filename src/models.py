c29ad98f901f8a219c7cdff5f913a7d96ad90a97
Emilio Gozo <emil.gozo@gmail.com>
2021-04-04 20:33:21 +0800

    chore: update REGION_MAP lookup

--- src/models.py
+++ src/models.py
@@ -5,7 +5,16 @@ from helpers import camel_case
 
 SEX_ENUM = ["male", "female"]
 REMOVAL_TYPE_ENUM = ["recovered", "died"]
-HEALTH_STATS_ENUM = ["recovered", "died", "confirmed", "asymptomatic", "mild", "moderate", "severe", "critical"]
+HEALTH_STATS_ENUM = [
+    "recovered",
+    "died",
+    "confirmed",
+    "asymptomatic",
+    "mild",
+    "moderate",
+    "severe",
+    "critical",
+]
 
 CASE_SCHEMA = dict(
     caseCode=dict(dtype="String"),
@@ -56,6 +65,23 @@ REGION_MAP = {
     "4B": "MIMAROPA (Region IV-B)",
     "MIMAROPA": "MIMAROPA (Region IV-B)",
     "BARMM": "Autonomous Region of Muslim Mindanao (ARMM)",
+    "REGION I (ILOCOS REGION)": "Ilocos Region (Region I)",
+    "REGION II (CAGAYAN VALLEY)": "Cagayan Valley (Region II)",
+    "REGION III (CENTRAL LUZON)": "Central Luzon (Region III)",
+    "REGION IV-A (CALABAR ZON)": "CALABARZON (Region IV-A)",
+    "REGION IV-B (MIMAROPA)": "MIMAROPA (Region IV-B)",
+    "REGION V (BICOL REGION)": "Bicol Region (Region V)",
+    "REGION VI (WESTERN VISAYAS)": "Western Visayas (Region VI)",
+    "REGION VII (CENTRAL VISAYAS)": "Central Visayas (Region VII)",
+    "REGION VIII (EASTERN VISAYAS)": "Eastern Visayas (Region VIII)",
+    "REGION IX (ZAMBOANGA PENINSULA)": "Zamboanga Peninsula (Region IX)",
+    "REGION X (NORTHERN MINDANAO)": "Northern Mindanao (Region X)",
+    "REGION XI (DAVAO REGION)": "Davao Region (Region XI)",
+    "REGION XII (SOCCSKSA RGEN)": "SOCCSKSARGEN (Region XII)",
+    "REGION XIII (CARAGA)": "Caraga (Region XIII)",
+    "AUTONOMOUS REGION IN MUSLIM MINDANAO (ARMM)": "Autonomous Region of Muslim Mindanao (ARMM)",
+    "CORDILLERA ADMINISTRA TIVE REGION (CAR)": "Cordillera Administrative Region (CAR)",
+    "NATIONAL CAPITAL REGION (NCR)": "Metropolitan Manila",
     "REPATRIATE": "Repatriate",
     "Repatriate": "Repatriate",
     "ROF": "ROF",
@@ -63,6 +89,7 @@ REGION_MAP = {
 }
 
 REGION_UNKNOWN = ["ROF", "Repatriate", ""]
+
 
 def prep_cases_df(df):
     df = df.rename(columns=CASE_FIELD_MAP)
@@ -74,19 +101,32 @@ def prep_cases_df(df):
             if CASE_SCHEMA[col_name]["dtype"] == "String":
                 if "choices" in CASE_SCHEMA[col_name]:
                     df[col_name] = df[col_name].str.strip().str.lower()
-                    no_match = df.dropna().loc[~df[col_name].isin(CASE_SCHEMA[col_name]["choices"]), col_name].unique()
+                    no_match = (
+                        df.dropna()
+                        .loc[
+                            ~df[col_name].isin(CASE_SCHEMA[col_name]["choices"]),
+                            col_name,
+                        ]
+                        .unique()
+                    )
                     if len(no_match) > 0:
-                        print(f"{col_name} has unmatched values: {no_match}!!! exiting...")
+                        print(
+                            f"{col_name} has unmatched values: {no_match}!!! exiting..."
+                        )
                         valid = False
                         break
                 if "default" in CASE_SCHEMA[col_name]:
-                    df.loc[df[col_name].isna(), col_name] = CASE_SCHEMA[col_name]["default"]
+                    df.loc[df[col_name].isna(), col_name] = CASE_SCHEMA[col_name][
+                        "default"
+                    ]
                 else:
                     df.loc[df[col_name].isna(), col_name] = ""
                 df[col_name] = df[col_name].str.strip()
             elif CASE_SCHEMA[col_name]["dtype"] == "Integer":
                 if "default" in CASE_SCHEMA[col_name]:
-                    df.loc[df[col_name].isna(), col_name] = CASE_SCHEMA[col_name]["default"]
+                    df.loc[df[col_name].isna(), col_name] = CASE_SCHEMA[col_name][
+                        "default"
+                    ]
                 else:
                     df.loc[df[col_name].isna(), col_name] = -1
                 df[col_name] = df[col_name].map(int, na_action="ignore")
@@ -97,7 +137,9 @@ def prep_cases_df(df):
             elif CASE_SCHEMA[col_name]["dtype"] == "Date":
                 df[col_name] = to_datetime(df[col_name]).dt.tz_localize("Asia/Manila")
                 if "default" in CASE_SCHEMA[col_name]:
-                    df.loc[df[col_name].isna(), col_name] = CASE_SCHEMA[col_name]["default"]
+                    df.loc[df[col_name].isna(), col_name] = CASE_SCHEMA[col_name][
+                        "default"
+                    ]
                 else:
                     df.loc[df[col_name].isna(), col_name] = 0
         else:
